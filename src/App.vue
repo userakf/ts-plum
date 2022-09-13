72dd215e7f6ca0a15ca504f3adde1818c4816b13
userakf <820279897@qq.com>
2022-09-13 21:21:09 +0800

    chore:tsjson

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,54 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue';
+
+const el = $ref<HTMLCanvasElement>()
+const ctx = $computed(() => el!.getContext('2d')!)
+const WIDTH = 400
+const HEIGHT = 400
+interface Point {
+  x: number,
+  y: number
+}
+interface Branch {
+  start: Point,
+  length: number,
+  theta: number
+}
+function lineTo(p1: Point, p2: Point) {
+  ctx.beginPath()
+  ctx.moveTo(p1.x, p1.y)
+  ctx.lineTo(p2.x, p2.y)
+  ctx.stroke()
+}
+function drawBranch(l: Branch) {
+  const { start, length, theta } = l
+  const end = {
+    x: start.x + length * Math.cos(theta),
+    y: start.y + length * Math.sin(theta)
+  }
+  lineTo(start, end)
+}
+function init() {
+  ctx.strokeStyle = '#fff'
+  const branch = {
+    start: { x: WIDTH / 2, y: HEIGHT },
+    length: 100,
+    theta: -Math.PI / 2
+  }
+  drawBranch(branch)
+}
+onMounted(() => {
+
+  init()
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <canvas ref="el" width="400" height="400"></canvas>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+canvas {
+  border: 1px solid #fff;
 }
 </style>
