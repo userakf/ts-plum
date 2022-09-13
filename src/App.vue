1a705a86006bd50e6954e1fe5ba531f7f9170547
userakf <820279897@qq.com>
2022-09-13 22:51:24 +0800

    chore:update

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,8 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-
-const el = $ref<HTMLCanvasElement>()
-const ctx = $computed(() => el!.getContext('2d')!)
-const WIDTH = 1000
-const HEIGHT = 600
-interface Point {
-  x: number,
-  y: number
-}
-interface Branch {
-  start: Point,
-  length: number,
-  theta: number
-}
-function lineTo(p1: Point, p2: Point) {
-  ctx.beginPath()
-  ctx.moveTo(p1.x, p1.y)
-  ctx.lineTo(p2.x, p2.y)
-  ctx.stroke()
-}
-function getEndPoint(b: Branch) {
-  return {
-    x: b.start.x + b.length * Math.cos(b.theta),
-    y: b.start.y + b.length * Math.sin(b.theta)
-  }
-}
-
-function drawBranch(b: Branch) {
-  lineTo(b.start, getEndPoint(b))
-}
-function init() {
-  ctx.strokeStyle = '#fff'
-  step({
-    start: { x: WIDTH / 2, y: HEIGHT },
-    length: 15,
-    theta: -Math.PI / 2
-  })
-}
-const pendingTask: Function[] = []
-function step(b: Branch, depth = 0) {
-
-  const end = getEndPoint(b)
-  drawBranch(b)
-  if (depth < 4 || Math.random() < 0.5) {
-    pendingTask.push(() => step({
-      start: end,
-      length: b.length + (Math.random() * 10 - 5),
-      theta: b.theta - 0.3 * Math.random()
-    }, depth + 1))
-  }
-  if (depth < 4 || Math.random() < 0.5) {
-    pendingTask.push(() => step({
-      start: end,
-      length: b.length + (Math.random() * 10 - 5),
-      theta: b.theta + 0.3 * Math.random()
-    }, depth + 1))
-  }
-
-}
-function frame() {
-  const tasks = [...pendingTask]
-  pendingTask.length = 0
-  tasks.forEach(fn => fn())
-}
-let framsCount = 0
-function startFrame() {
-  requestAnimationFrame(() => {
-    framsCount += 1
-    if (framsCount % 3 === 0)
-      frame()
-    startFrame()
-  })
-}
-startFrame()
-onMounted(() => {
-  init()
-})
 </script>
 
 <template>
-  <canvas ref="el" width="1000" height="600"></canvas>
+  <Plum />
 </template>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
-
+import Components from 'unplugin-vue-components/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -10,6 +10,12 @@ export default defineConfig({
     }),
     AutoImport({
       imports:['vue'],
+      dts: 'src/auto-import.d.ts'
+    }),
+    Components({
+      dirs:['src/components'],
+      extensions:['vue'],
+      dts:'src/components.d.ts'
     })
   ]
 })
